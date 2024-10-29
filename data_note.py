4a52e2250c3edcf78400ba7c37d74dbef2bf4ad4
Ron Wohler <Rwohlerr@gmail.com>
2024-10-29 17:14:25 +0000

    first commit

---
+++ data_note.py
@@ -0,0 +1,48 @@
+# The print()
+# print("I can white sentences here")
+
+# print("A string is a collection of characters")
+# print("12 + 34")
+
+# Integers aare the whole numbers. They have maathemaatic values, I can use operators on them.
+# print(1234)
+# print(12 + 34)
+
+# Floating points are fractional numbers. 
+# print(12.34)
+
+# None is blank data.
+# print(None)
+
+# #Boolean - True or False, yes or no, on or off.
+# print(True)
+# print(False)
+
+
+# print("hello world".upper())
+
+# print(len("hello world")) - Will give the length of the string which is 11.
+
+# print("hello world"[1]) - Will print the character indexed as 1 which is e. (python uses a Zero Index beginning labeling from zero 0-10)
+
+# print("hello world"[-1]) -  Will print the character indexed as last in a string which is d.
+
+# print("   |   |   ")
+# print("-----------")
+# print("   |   |   ") # Will print a grid
+# print("-----------")
+# print("   |   |   ")
+
+# or
+
+# print("the quick brown DOG".lower()) # Will make text lowercase i.e. DOG into dog.
+# print("the quick BROWN fox. hello".capitalize()) # Will capitalize the first character and turn all other text into lowercase.
+
+# print("the quick brown foxfoxfox".count("fox")) # Will return all instances of fox, in this case it will return 3.
+
+# print("the quick brown fox".find(e)) # Will return the index location of e with a string.
+
+# print("the quick brown fox".replace("fox", "frog")) # Will replace characters with others, in this example it will produce  "the quick brown frog"
+
+
+# print("   the quick brown fox        ".strip()) # Will remove the spaces at the edges of data
---
+++ test.py
@@ -0,0 +1,7 @@
+print("hello, world!")
+
+# print("this is another line of text")
+
+# print("hello, this is my third line of code")
+
+# print("hello world".upper())
